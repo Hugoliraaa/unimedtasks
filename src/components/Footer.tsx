3dbd1033115ed2ed041d8b851dc993999b0478a6
Hugoliraaa <hugo.marx58@gmail.com>
2024-02-27 08:56:26 -0300

    up6.2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <footer className="text-center py-6 font-medium">UnimedJP</footer>;
+  return <footer className="text-center py-6 font-medium">Suporte / Infraestrutura</footer>;
 };
 
 export default Footer;
