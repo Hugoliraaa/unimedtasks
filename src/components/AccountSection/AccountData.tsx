ea7005b88eac0dbf396bedde8a8ddd7ed448150d
Hugoliraaa <hugo.marx58@gmail.com>
2024-02-28 12:06:00 -0300

    upfooter

--- src/components/AccountSection/AccountData.tsx
+++ src/components/AccountSection/AccountData.tsx
@@ -36,7 +36,7 @@ const AccountData: React.FC = () => {
           href=""
           className="mt-4 bg-rose-100 p-2 rounded-md text-rose-600 text-center transition hover:bg-rose-200 dark:bg-slate-700/[.3] dark:text-slate-200"
         >
-          UNIMED JOÃO PESSSOA
+          UNIMED JOÃO PESSOA
         </a>
       </section>
     </LayoutMenus>
